85d83edf0f5ee56b9792c53143af18b41a1e9f59
bemja28 <bemja28091991@gmail.com>
2022-06-21 12:33:56 -0300

    volver

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(express.static("public"));
 
 app.get("/", (req,res) => res.sendFile(path.join(__dirname, "views", "index.html")));
 app.get("/babbage", (req,res) => res.sendFile(path.join(__dirname, "views", "babbage.html")));
-app.get("/berbers-lee", (req,res) => res.sendFile(path.join(__dirname, "views", "berbers-lee.html")));
+app.get("/berners-lee", (req,res) => res.sendFile(path.join(__dirname, "views", "berners-lee.html")));
 app.get("/clarke", (req,res) => res.sendFile(path.join(__dirname, "views", "clarke.html")));
 app.get("/hamilton", (req,res) => res.sendFile(path.join(__dirname, "views", "hamilton.html")));
 app.get("/hopper", (req,res) => res.sendFile(path.join(__dirname, "views", "hopper.html")));
